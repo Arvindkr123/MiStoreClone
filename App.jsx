ac7df11ae5396340add644a60b34fd58143ffba0
Arvind <thakurarvindkr10@gmail.com>
2023-06-02 17:14:51 +0530

    Accessories done

--- App.jsx
+++ App.jsx
@@ -2,26 +2,87 @@ import React from "react";
 import "./App.css";
 import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import data from "./data/data.json";
 import Slider from "./components/Slider";
 import Offers from "./components/Offers.jsx";
 import Heading from "./components/Heading.jsx";
 import StarProducts from "./components/StarProducts.jsx";
 import HotAccessoriesMenu from "./components/HotAccessoriesMenu.jsx";
+import HotAccessories from "./components/HotAccessories.jsx";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <PreNavbar />
-      <Navbar />
-      <Slider start={data.banner.start} />
-      <Offers offer={data.offer} />
-      <Heading text={"STAR PRODUCTS"} />
-      <StarProducts startProduct={data.starProduct} />
-      <Heading text={"HOT ACCESSORIES"} />
-      <HotAccessoriesMenu/>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <PreNavbar />
+        <Navbar />
+        <Slider start={data.banner.start} />
+        <Offers offer={data.offer} />
+        <Heading text={"STAR PRODUCTS"} />
+        <StarProducts startProduct={data.starProduct} />
+        <Heading text={"HOT ACCESSORIES"} />
+        <HotAccessoriesMenu />
+        <Routes>
+          <Route
+            exact
+            path="/music"
+            element={
+              <HotAccessories
+                music={data.hotAccessories.music}
+                musicCover={data.hotAccessoriesCover.music}
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/smartDevice"
+            element={
+              <HotAccessories
+                smartDevice={data.hotAccessories.smartDevice}
+                smartDeviceCover={data.hotAccessoriesCover.smartDevice}
+              />
+            }
+          ></Route>
+
+          <Route
+            exact
+            path="/home"
+            element={
+              <HotAccessories
+                home={data.hotAccessories.home}
+                homeCover={data.hotAccessoriesCover.home}
+              />
+            }
+          ></Route>
+
+          <Route
+            exact
+            path="/lifestyle"
+            element={
+              <HotAccessories
+                lifeStyle={data.hotAccessories.lifeStyle}
+                lifeStyleCover={data.hotAccessoriesCover.lifeStyle}
+              />
+            }
+          ></Route>
+
+          <Route
+            exact
+            path="/mobileAccessories"
+            element={
+              <HotAccessories
+                mobileAccessories={data.hotAccessories.mobileAccessories}
+                mobileAccessoriesCover={
+                  data.hotAccessoriesCover.mobileAccessories
+                }
+              />
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
