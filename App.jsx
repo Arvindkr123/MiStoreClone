50f8faad9708df29d70aaff177ba49dcbb6ac5b9
Arvind <thakurarvindkr10@gmail.com>
2023-06-02 12:07:34 +0530

    PreNabar done

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import PreNavbar from './components/PreNavbar'
+import './Style/PreNavbar.css'
 
 const App = () => {
     return (
         <div>
-            
+            <PreNavbar/>
         </div>
     )
 }
